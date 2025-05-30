e99f27f0341b65aa31d6acfc06e3ba57a964c4fd
NotInvented <notinvented01@gmail.com>
2025-05-30 22:47:51 +0300

    changed default template for editor

--- src/shared/hooks/useEditorContent.ts
+++ src/shared/hooks/useEditorContent.ts
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { formatHtml } from '../utils/htmlFormatter';
 
-export const useEditorContent = (initialContent: string = '<p>Hello World! This is a TipTap editor.</p>') => {
+export const useEditorContent = (initialContent: string = '<section id="prologue" data-toc-title="Prologue" data-section-type="chapter"><section id="prologue_intro" data-toc-title="Intro" data-section-type="part"><p></p></section></section>') => {
   const [content, setContent] = useState(initialContent);
   const [formattedHtml, setFormattedHtml] = useState('');
 
